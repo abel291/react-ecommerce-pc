430616a7eb8c166cf128d9968ba82a9f67d9f7aa
Abel <abel291m@gmail.com>
2021-12-31 01:59:02 -0500

    change api url

--- src/auth/apiClient.js
+++ src/auth/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const apiClient = axios.create({
-    baseURL: "https://laravel-api-abel291.herokuapp.com/api",
+    baseURL: "https://laravel-api-ecommerce-pc.herokuapp.com/api",
     //withCredentials: true,
 })
 
